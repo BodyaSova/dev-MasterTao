a5d5404c9ef6486568332da39d7620b48efe92cb
BodyaSova <bod.adamenko@gmail.com>
2024-01-14 16:19:18 +0200

    step: Mobile adaptive

---
+++ dev/assets/js/burger-menu.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector('.btn-burger')
+const nav = document.querySelector('.header__nav')
+
+function burgerMenu () {
+    burger.classList.toggle('open');
+    nav.classList.toggle('show');
+}
+
+burger.addEventListener('click', burgerMenu);
